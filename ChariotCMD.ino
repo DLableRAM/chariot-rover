38fa6ead2214211f2394d6882016a4eaaa81a528
Michael Ashford Bevens <ashfordm@my.erau.edu>
2024-01-10 08:42:19 -0700

    forgot quotations

--- ChariotCMD.ino
+++ ChariotCMD.ino
@@ -1,1 +1,2 @@
 #include "consts.h"
+#include "classes.h"
--- consts.h
+++ consts.h
@@ -1,1 +1,9 @@
 //Constants go here
+
+//Layer parameters: dictates the LIDAR point count
+#define LAYERCOUNT 10
+#define LAYERRES 10
+
+//Angle parameters": Angle behaviors for LIDAR
+#define INITANGLE 10
+#define ENDANGLE 10
