3a986e410e213f650bf5721cf4912112cb2638b1
Michael Ashford Bevens <ashfordm@my.erau.edu>
2024-01-12 08:37:53 -0700

    Added IMU processing in IMU management class.

--- classes.h
+++ classes.h
@@ -30,10 +30,30 @@ class IMUmanager {
     //When I remember to add that dependency, anyway!
     public:
         IMUdata IMUprocess() {
-            //integrate gyro, return IMUdata
-            //Also include alpha filter
-            //uh, I wrote this code before, I'll go find it later
-            IMUdata output;
+            IMUdata IMUd;
+            float gx, gy, gz;
+            unsigned long curTime = millis();
+            unsigned long dt = curTime - lastTime;
+            if (IMU.gyroscopeAvailable() && IMU.accelerationAvailable()) {
+                IMU.readGyroscope(gx, gy, gz);
+                IMU.readAcceleration(IMUd.ax, IMUd.ay, IMUd.az);
+            }
+
+            float theta_gy = lastThetaY + gy*((float)dt/1000.0);
+            float theta_ay = RAD_TO_DEG*atan2(IMUd.ax,IMUd.az);
+            IMUd.gy = alpha*theta_gy + (1.0-alpha)*theta_ay;
+            lastThetaY = IMUd.gy;
+
+            float theta_gx = lastThetaX + gx*((float)dt/1000.0);
+            float theta_ax = RAD_TO_DEG*atan2(IMUd.ay,IMUd.az);
+            IMUd.gx = alpha*theta_gx + (1.0-alpha)*theta_ax;
+            lastThetaX = IMUd.gx;
+
+            float theta_gz = lastThetaZ + gz*((float)dt/1000.0);
+            float theta_az = RAD_TO_DEG*atan2(IMUd.ay,IMUd.ax);
+            IMUd.gz = alpha*theta_gz + (1.0-alpha)*theta_az;
+            lastThetaZ = IMUd.gz;
+            return IMUd;
         }
 };
 
